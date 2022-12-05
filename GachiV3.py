bb870f5ff247cd76622f5b589e6ae66738de66e5
Danil <nnaumov538@gmail.com>
2022-12-06 01:02:09 +0500

    Add code

---
+++ GachiV3.py
@@ -0,0 +1,32 @@
+# Гачи Version 3 
+ 
+from datetime import datetime 
+ 
+name = input("\nEnter name \n\n>>> ") 
+password = input("\nEnter password \n\n>>> ") 
+worldcs = input("\nWcs ? \n\n>>> ") 
+ 
+def gachi(password, worldcs): 
+	if password == "world cum shot": 
+		if worldcs == "YES" or worldcs == "Yes" or worldcs == "yes": 
+			time = datetime.now().hour 
+			if 7 <= time <= 23: 
+				print(f"\nNow {time} hours, mef day") 
+				return f"\n♂️ Thanks for using, {name} ♂️" 
+				exit() 
+			else: 
+				print(f"\nNow {time} hours, we sleep ZzZz...") 
+				return f"\n♂️ Thanks for using, {name} ♂️" 
+				exit() 
+		else: 
+			worldcs != "YES" or worldcs != "Yes" or worldcs != "yes" 
+			print("\nFUCK YOU LEATHER MAN!!!") 
+			exit()	 
+	else:
+		password != "world cum shot"
+		print(f"{password} - Not a valid password") 
+		exit() 
+	return f"\n♂️ Thanks for using, {name} ♂️" 
+ 
+result = gachi(password, worldcs) 
+print(result)
